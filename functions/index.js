276a005f6ccc24939155960721f92605e53a6a5c
KJ <cozykoji18@gmail.com>
2020-06-23 03:39:10 +0900

    ユーザー登録functionを作成

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,28 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+  databaseURL: "https://monnaka-app.firebaseio.com"
+});
+
+exports.registerUsers = functions.auth.user().onCreate(user => {
+  const { uid } = user;
+  const db = admin.firestore();
+  const displayName = user.displayName || "Anonymous";
+  const photoURL = user.photoURL || "/assets/img/default_profile.svg";
+
+  return db
+    .collection("users")
+    .doc(uid)
+    .set({
+      displayName: displayName,
+      photoURL: photoURL
+    });
+  //   .then(function() {
+  //   console.log("Document successfully written!");
+  // })
+  //   .catch((err) => {
+  //     console.log(err); // eslint-disable-line no-console
+  //   });
+});
