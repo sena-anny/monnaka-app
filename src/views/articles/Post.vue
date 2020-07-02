359ee22d2e827608c7e2cbbf908fbc813e1586d5
KJ <cozykoji18@gmail.com>
2020-07-03 01:42:44 +0900

    投稿画面の実装

--- src/views/articles/Post.vue
+++ src/views/articles/Post.vue
@@ -1,11 +1,34 @@
 <template>
-  <div>POST</div>
+  <div class="post">
+    <Menu></Menu>
+    <h2>投稿画面</h2>
+    <!-- タイトル -->
+    <!-- 本文 -->
+    <!-- タグ -->
+    <!-- 写真 -->
+  </div>
 </template>
 
 <script>
+import Menu from "@/components/Menu.vue";
+
 // firestoreに投稿内容をアップロードする
 // 写真はgoogle storage利用する
-export default {};
+export default {
+  name: "Home",
+  components: {
+    Menu
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    postData: function() {
+      // 投稿内容登録
+      // createdAt updatedAt uid
+    }
+  }
+};
 </script>
 
 <style lang="sass" scoped></style>
